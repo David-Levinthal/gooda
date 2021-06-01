1831876818c915933572346d1032cb01ccdad295
David Levinthal Ph.D <David-Levinthal@users.noreply.github.com>
2021-06-01 15:11:00 -0700

    update for loops

--- kernels/c2c_core_scan.sh
+++ kernels/c2c_core_scan.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 ulimit -n 32768
 cores=$(cat /proc/cpuinfo | grep family | wc -l)
+((cores=cores-1))
 echo $cores cores
-for i in {2..128..2}; do
-	echo ./snoop_test/snoop_test -i0 -r1 -w$i -s $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan_s.log 2>&1
-	./snoop_test/snoop_test -i0 -r1 -w$i -s $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan_s.log 2>&1
+for i in $( eval echo "{2..$cores}"); do
+	echo ./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.test 2>&1
+#	./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.log 2>&1
 done
--- kernels/dram_core_scan.sh
+++ kernels/dram_core_scan.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 ulimit -n 32768
 cores=$(cat /proc/cpuinfo | grep family | wc -l)
+((cores=cores-1))
 echo $cores cores
-for i in {2..128..2}; do
-	./latency_noarch/walker -i0 -r$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> latency_scan.log 2>&1
+for i in $( eval echo "{1..$cores}"); do
+#	./latency_noarch/walker -i0 -r$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> latency_scan.log 2>&1
+	echo $i
 done
