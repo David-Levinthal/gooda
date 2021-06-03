4373276669045e1cbe1a2e13d99212c498f1423e
David Levinthal Ph.D <David-Levinthal@users.noreply.github.com>
2021-06-03 12:53:50 -0700

    remove #

--- kernels/c2c_core_scan.sh
+++ kernels/c2c_core_scan.sh
@@ -5,5 +5,5 @@ cores=$(cat /proc/cpuinfo | grep family | wc -l)
 echo $cores cores
 for i in $( eval echo "{2..$cores}"); do
 	echo ./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.test 2>&1
-#	./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.log 2>&1
+	./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.log 2>&1
 done
--- kernels/dram_core_scan.sh
+++ kernels/dram_core_scan.sh
@@ -4,6 +4,6 @@ cores=$(cat /proc/cpuinfo | grep family | wc -l)
 ((cores=cores-1))
 echo $cores cores
 for i in $( eval echo "{1..$cores}"); do
-#	./latency_noarch/walker -i0 -r$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> latency_scan.log 2>&1
-	echo $i
+	./latency_noarch/walker -i0 -r$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> latency_scan.log 2>&1
+
 done
