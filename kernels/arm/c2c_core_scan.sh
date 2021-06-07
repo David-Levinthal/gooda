cef1d5b8659aac72470584daa1090d865cea30f1
David Levinthal Ph.D <David-Levinthal@users.noreply.github.com>
2021-06-07 13:28:21 -0700

    log invocation

--- kernels/arm/c2c_core_scan.sh
+++ kernels/arm/c2c_core_scan.sh
@@ -4,6 +4,6 @@ cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 ((cores=cores-1))
 echo $cores cores
 for i in $( eval echo "{2..$cores}"); do
-	echo ./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.test 2>&1
+	echo ./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.log 2>&1
 	./snoop_test/snoop_test -i0 -r1 -w$i $1 $2 $3 $4 $5 $6 $7 $8 $9 >> c2c_latency_scan.log 2>&1
 done
