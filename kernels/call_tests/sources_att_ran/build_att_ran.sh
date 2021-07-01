9ca38d14cc470d870c0216c0100389d3a3178f66
David Levinthal Ph.D <David-Levinthal@users.noreply.github.com>
2021-07-01 15:16:29 -0700

    fix binary size, add second test

--- kernels/call_tests/sources_att_ran/build_att_ran.sh
+++ kernels/call_tests/sources_att_ran/build_att_ran.sh
@@ -3,6 +3,6 @@ ulimit -s 65536
 export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
 gcc -o generator_att_ran generator_att_ran.c
 ./generator_att_ran $1 $2 $3
-gcc -O0 -g -c FOO_[0-9]*.c
-gcc -O0 -g -c FOO_main.c
-gcc -O0 -g FOO_main.o FOO_0*.o -o FOO_static
+gcc -O0 -Os -c FOO_[0-9]*.c
+gcc -O0 -Os -c FOO_main.c
+gcc -O0 -Os FOO_main.o FOO_0*.o -o FOO_static
---
+++ kernels/call_tests/sources_att_ran/build_att_ran2.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+ulimit -s 65536
+export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+gcc -o generator_att_ran2 generator_att_ran2.c
+./generator_att_ran2 $1 $2 $3
+gcc -O0 -Os -c FOO_[0-9]*.c
+gcc -O0 -Os -c FOO_main.c
+gcc -O0 -Os FOO_main.o FOO_0*.o -o FOO_static2
