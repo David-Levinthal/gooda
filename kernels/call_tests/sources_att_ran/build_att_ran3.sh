bd83445a1de6ee2271b3e84e403b7b2dcd63e278
David Levinthal Ph.D. <David-Levinthal@users.noreply.github.com>
2025-09-16 10:57:18 -0700

    Add files via upload

---
+++ kernels/call_tests/sources_att_ran/build_att_ran3.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+ulimit -s 65536
+export LD_LIBRARY_PATH=./:$LD_LIBRARY_PATH
+gcc -o generator_att_ran3 generator_att_ran3.c
+./generator_att_ran3 $1 $2 $3
+make -f Makefile3 -j 40
+#gcc -O0 -Os -c FOO_[0-9]*.c
+#gcc -O0 -Os -c FOO_main.c
+#gcc -O0 -Os FOO_main.o FOO_0*.o -o FOO_static2
