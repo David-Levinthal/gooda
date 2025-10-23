e26374cebb1cd1245c943d93df05c1432b16d79e
David Levinthal Ph.D. <David-Levinthal@users.noreply.github.com>
2025-10-23 11:07:07 -0700

    Update generator_att.c

--- kernels/call_tests/sources_att/generator_att.c
+++ kernels/call_tests/sources_att/generator_att.c
@@ -27,7 +27,7 @@ static int genC (int star_so, int chain, int depth, FILE* fp, FILE* fp_header)
 	fprintf (fp,"\t__asm__( \n");
 
 	for(j=0;j<50;j++){
-		fprintf (fp,"\t\"xorq    %%rdx, %%rdx\\n\\t\"\n");
+		fprintf (fp,"\t\"xorq    %%r10, %%r10\\n\\t\"\n");
 		}
 	fprintf (fp,"\t);\n");
 
@@ -118,4 +118,5 @@ main(int argc, char* argv[])
 	rval = create_source(max_so,max_chain,max_depth);
 	rval = create_main(max_so,max_chain,max_depth);
 }
+
 
