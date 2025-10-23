767ebe0126f0770a71c12b5f59b21ec4324120ee
David Levinthal Ph.D. <David-Levinthal@users.noreply.github.com>
2025-10-23 11:03:53 -0700

    Update generator_att2.c

--- kernels/call_tests/sources_att/generator_att2.c
+++ kernels/call_tests/sources_att/generator_att2.c
@@ -103,7 +103,7 @@ static int create_main(int max_so, int max_chain, int max_depth)
 
 	return 0;
 }
-
+int
 main(int argc, char* argv[])
 {
 	int max_so,max_chain,max_depth, rval;
@@ -119,3 +119,4 @@ main(int argc, char* argv[])
 	rval = create_source(max_so,max_chain,max_depth);
 	rval = create_main(max_so,max_chain,max_depth);
 }
+
